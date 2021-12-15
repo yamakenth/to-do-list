949290c91fe9268dcd9e66681bd9cce14a789946
Ken Yamada <yamakenth@gmail.com>
2021-12-15 14:22:46 +0700

    Add more properties to to do item class

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // to do item 
 class ToDoItem {
   // constructor 
-  constructor(title) {
-    this.title = title;
+  constructor(title, tag, details, dueDate, priority) {
     this.isCompleted = false;
+    this.title = title;
+    this.tag = tag;
+    this.details = details;
+    this.dueDate = dueDate;
+    this.priotity = priority;
   }
   // toggle complete status 
   toggleIsCompleted() {
@@ -27,8 +31,10 @@ class ToDoList {
 // test 
 const toDoList = new ToDoList();
 
-const item1 = new ToDoItem('wash car');
-const item2 = new ToDoItem('buy groceries');
+const item1 = new ToDoItem('wash car', 'chores', 'wash then wax car', '2021-12-17', 'low');
+const item2 = new ToDoItem('buy groceries', 'chores', 'cabbage, bread, beer', '2021-12-15', 'high');
+const item3 = new ToDoItem('math assignment 3', 'school', 'problem set 1 - 6', '2021-12-23', 'med');
 
 toDoList.addItemToList(item1);
 toDoList.addItemToList(item2);
+toDoList.addItemToList(item3);
