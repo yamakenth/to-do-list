a8cf2d6ed7b00feb65059a4ff73d4549207d31cc
Ken Yamada <yamakenth@gmail.com>
2021-12-15 14:13:19 +0700

    Create to-do item adn to-do list classes

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-// to-do item class 
-console.log('skrt');
+// to do item 
+class ToDoItem {
+  // constructor 
+  constructor(title) {
+    this.title = title;
+    this.isCompleted = false;
+  }
+  // toggle complete status 
+  toggleIsCompleted() {
+    (this.isCompleted) ? this.isCompleted = false : this.isCompleted = true;
+  }
+}
+
+// to do list 
+class ToDoList {
+  // constructor 
+  constructor() {
+    this.list = [];
+  }
+  // add item to list 
+  addItemToList(item) {
+    this.list.push(item);
+  }
+
+}
+
+// test 
+const toDoList = new ToDoList();
+
+const item1 = new ToDoItem('wash car');
+const item2 = new ToDoItem('buy groceries');
+
+toDoList.addItemToList(item1);
+toDoList.addItemToList(item2);
