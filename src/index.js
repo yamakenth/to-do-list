7a72e8a4444c1be0c4a43fae68794852b1924fd0
Ken Yamada <yamakenth@gmail.com>
2021-12-15 17:30:31 +0700

    Add add button display

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { createHeader } from "./header";
 import { createDisplay } from "./display";
+import { createAddButton } from "./addbutton";
 import './style.css';
 
 const header = document.querySelector('.header');
 createHeader(header);
+
+const addButton = document.querySelector('.add-button');
+createAddButton(addButton);
 
 const display = document.querySelector('.display');
 createDisplay(display);
