0f40d7b95c6878d2f33b9fc00794aed10ac7a140
Ken Yamada <yamakenth@gmail.com>
2021-12-15 19:51:45 +0700

    Add ability to submit form and updat to do list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createHeader } from "./header";
 import { createDisplay } from "./display";
 import { createAddButton } from "./addbutton";
+import { toDoList } from "./app";
 import './style.css';
 
 const header = document.querySelector('.header');
@@ -12,11 +13,43 @@ createAddButton(addButton);
 const display = document.querySelector('.display');
 createDisplay(display);
 
+const overlay = document.querySelector('#overlay');
+const form = document.querySelector('.new-item-form');
 const add = document.querySelector('.add-button button');
 add.addEventListener('click', () => {
-  console.log('clicked');
-  const overlay = document.querySelector('#overlay');
   overlay.classList.add('active');
-  const form = document.querySelector('.new-item-form');
   form.classList.add('active');
+});
+
+const closeButtons = document.querySelectorAll('.form-close');
+closeButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    form.classList.remove('active');
+  });
+});
+
+const newTitle = document.querySelector("#new-title");
+const newDetails = document.querySelector("#new-details");
+const newDueDate = document.querySelector("#new-due-date");
+const newPriority = document.querySelector("#new-priority");
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newTitleValue = newTitle.value;
+  newTitle.value = '';
+
+  const newDetailsValue = newDetails.value;
+  newDetails.value = '';
+
+  const newDueDateValue = newDueDate.value;
+  newDueDate.value = '';
+
+  const newPriorityValue = newPriority.value;
+  newPriority.value = '';
+
+  overlay.classList.remove('active');
+  form.classList.remove('active');
+
+  toDoList.addNewItemToList(newTitleValue, 'tag', newDetailsValue, newDueDateValue, "med");
+  console.log(toDoList.list);
 });
