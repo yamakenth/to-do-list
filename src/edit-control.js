d6b27b7b9eee9cdbac87b84bfe2381d8c7a4e839
Ken Yamada <yamakenth@gmail.com>
2021-12-16 19:30:48 +0700

    Add ability to show details of existing items

--- src/edit-control.js
+++ src/edit-control.js
@@ -5,6 +5,7 @@ import { addToggleControl } from './toggle-control';
 // take in document 
 // return no results 
 function addEditability(document, toDoList) {
+  // delete 
   const deleteButtons = document.querySelectorAll('.delete-button');
   deleteButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -14,6 +15,28 @@ function addEditability(document, toDoList) {
       updateDisplay(toDoList);
       addToggleControl(document, toDoList);
       addEditability(document, toDoList);
+    });
+  });
+
+  // view detail/edit 
+  const overlay = document.querySelector('#overlay');
+  const newItemForm = document.querySelector('.new-item-form');
+  const detailButtons = document.querySelectorAll('.detail-button');
+  detailButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const currItem = toDoList.list[e.target.parentNode.parentNode.parentNode.dataset.indexNumber];
+      
+      overlay.classList.add('active');
+      newItemForm.classList.add('active');
+      newItemForm.querySelector('#new-title').select();
+
+      newItemForm.querySelector('#new-title').value = currItem.title;
+      newItemForm.querySelector('#new-project').value = currItem.project;
+      newItemForm.querySelector('#new-description').value = currItem.description;
+      newItemForm.querySelector('#new-due-date').value = currItem.dueDate;
+      newItemForm.querySelector(`#${currItem.priority}`).checked = true;
+
+
     });
   });
 
