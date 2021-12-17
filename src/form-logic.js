d5f06ee498f96bc92cec0e249e64e83678699ebb
Ken Yamada <yamakenth@gmail.com>
2021-12-17 14:25:00 +0700

    Add functionality to clear form input fields on close

--- src/form-logic.js
+++ src/form-logic.js
@@ -4,9 +4,21 @@
 function addEventListenerToCloseButton() {
   const closeButton = document.querySelector('.close-button');
   closeButton.addEventListener('click', () => {
+    resetInputFields();
     document.querySelector('#overlay').classList.remove('active');
     document.querySelector('.new-item-form').classList.remove('active');
   });
+}
+
+// reset input fields for new input 
+// take in form
+// return no results 
+function resetInputFields() {
+  document.querySelector('#new-title').value = '';
+  document.querySelector('#new-project').value = '';
+  document.querySelector('#new-description').value = '';
+  document.querySelector('#new-due-date').value = null;
+  document.querySelector('#medium').checked = true;
 }
 
 export { addEventListenerToCloseButton };
