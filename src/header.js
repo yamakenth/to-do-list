450141d1714cc3bed59e22ab165135b3a8b46ec6
Ken Yamada <yamakenth@gmail.com>
2021-12-19 11:52:15 +0700

    Add button to display dummy data in header

--- src/header.js
+++ src/header.js
@@ -7,6 +7,25 @@ function createHeader() {
   // > header div 
   const headerContainer = document.createElement('div');
   headerContainer.classList.add('header');
+  // >> left contaier 
+  const leftContainer = createLeftContainer();
+  // >> right contaier 
+  const rightContainer = createRightContainer();
+
+  // append child to parent 
+  headerContainer.appendChild(leftContainer);
+  headerContainer.appendChild(rightContainer);
+  
+  document.querySelector('body').appendChild(headerContainer);
+}
+
+// create left container 
+// take in no parameters 
+// return <div class="left-container"></div>
+function createLeftContainer() {
+  // > container
+  const leftContainer = document.createElement('div');
+  leftContainer.classList.add('left-container');
   // >> logo 
   const logo = new Image();
   logo.src = Logo;
@@ -17,9 +36,29 @@ function createHeader() {
   title.textContent = 'To Do List';
 
   // append child to parent 
-  headerContainer.appendChild(logo);
-  headerContainer.appendChild(title);
-  document.querySelector('body').appendChild(headerContainer);
+  leftContainer.appendChild(logo);
+  leftContainer.appendChild(title);
+
+  return leftContainer;
+}
+
+// create right container 
+// take in no parameters 
+// return <div class="right-container"></div>
+function createRightContainer() {
+  // > container 
+  const rightContainer = document.createElement('div');
+  rightContainer.classList.add('right-container');
+  // > button 
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.classList.add('dummy-data-button');
+  button.textContent = 'Populate list with dummy data';
+  
+  // append child to parent 
+  rightContainer.appendChild(button);
+  
+  return rightContainer;
 }
 
 export { createHeader };
